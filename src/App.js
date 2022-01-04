8fec1ac1310b57bc5756ca6b1ce566ad41d5a682
hkava <hkava@tvo.org>
2022-01-04 11:27:09 -0500

    read from json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Carousel from './components/Carousel';
-import { Data } from './components/Data';
+// import { Data } from './components/Data';
+import Test from './widgets/test.json'
 
 function App() {
   return (
     <main className="flexbox">
+        {/* <Carousel
+          items={Data} /> */}
         <Carousel
-          items={Data} />
+          items={Test.data} />
+          {/* {console.log(Test.data)} */}
     </main>
   );
 }
