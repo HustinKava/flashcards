54c7729409c06dee9c4fb99819fe4e9bbbdca7f7
hkava <hkava@tvo.org>
2022-01-04 11:33:25 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 import Carousel from './components/Carousel';
-// import { Data } from './components/Data';
-import Test from './widgets/test.json'
+import Flashcards from './widgets/flashcards1.json'
 
 function App() {
   return (
     <main className="flexbox">
-        {/* <Carousel
-          items={Data} /> */}
         <Carousel
-          items={Test.data} />
-          {/* {console.log(Test.data)} */}
+          items={Flashcards.data} />
     </main>
   );
 }
